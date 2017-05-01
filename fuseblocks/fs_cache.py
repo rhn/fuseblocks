6ed4af62525dedd22e19558352fdb9bdaec22f8e
rhn <gihu.rhn@porcupinefactory.org>
2017-05-01 19:29:41 +0200

    Added caching of open() calls

--- fuseblocks/fs_cache.py
+++ fuseblocks/fs_cache.py
@@ -1,3 +1,7 @@
+"""Contains caching primitives for avoiding expensive fetching of data from underlying layers.
+Currently works best when data of an input file is transformed in an expensive way into a different representation, with a different size.
+"""
+
 import os
 import hashlib
 import tempfile
@@ -8,8 +12,12 @@ from .base import OpenFile, FileLike, VirtStat
 from .realfs import FSFile
 from .passthrough import Passthrough
 
+import logging
+logger = logging.getLogger('fuseblocks.fs_cache')
+
 
 class CachedFSFile(FSFile):
+    """Allows to use the cached file for supplying information that must be generated."""
     def get_size(self):
         return VirtStat.from_stat(os.fstat(self.fd)).st_size
 
@@ -19,7 +27,6 @@ class FSStore:
     Stores data as files with filenames matching their md5 hash.
     
     Mappings matches path to hash.
-    TODO: reach for the underlying storage of the file doing the processing and hash what's there (requires file cooperation).
     TODO: delete when overflowing
     TODO: watch for changes.
     """
@@ -36,13 +43,13 @@ class FSStore:
             hash_ = self.hashes[path]
         except KeyError:
             return None
-        print("run cache hit")
+        logger.debug("run cache hit")
         cache_path = os.path.join(self.path, hash_)
         return self.OpenFile(cache_path, os.O_RDONLY)
 
     def rehash(self, path, backing):
         """Refresh parent's hash and return if cached version found"""
-        print("rehashing")
+        logger.info("rehashing")
         halg = self.HashAlg()
         with FileLike(backing) as in_stream:
             while True:
@@ -60,7 +67,7 @@ class FSStore:
 
     def update(self, path, src):
         """Regenerate cache contents. Expects the hash is already known."""
-        print("updating cache")
+        logger.info("updating cache")
         hash_ = self.hashes[path]
         with FileLike(src) as in_stream:
             with tempfile.NamedTemporaryFile(mode='w+b', dir=self.path, delete=False) as dest:
@@ -88,21 +95,28 @@ class DataCache(Passthrough):
         """parent - underlying block"""
         self.store = self.Store(self.CACHE_PATH)
         Passthrough.__init__(self, parent)
-    
+
     def getattr(self, path):
         ret = Passthrough.getattr(self, path)
         if stat.S_ISDIR(ret.st_mode):
             return ret
-
-        cached = self.store.get(path)
-        if cached is None:
-            # ASSUMPTION: parent transforms file data but does not create any
-            # ASSUMPTION: parent does not change paths
-            # these assumptions allow us to reach for parent.parent.open directly
-            # A more elegant solution would implement a "datasource" interface on cacheable transformation blocks.
-            cached = self.store.rehash(path, self.parent.datasource.open(path, os.O_RDONLY))
-            if cached is None:
-                cached = self.store.update(path, Passthrough.open(self, path, os.O_RDONLY))
+        f = self.open(path, os.O_RDONLY)
         ret = VirtStat.from_stat(ret)
-        ret.st_size = cached.get_size()
+        ret.st_size = f.get_size()
         return ret
+
+    def open(self, path, flags):
+        if flags & (os.O_WRONLY | os.O_RDWR):
+            raise NotImplementedError("Only reading supported.")
+        cached = self.store.get(path)
+        if cached is not None:
+            return cached
+        # ASSUMPTION: parent transforms file data but does not create any
+        # ASSUMPTION: parent does not change paths
+        # these assumptions allow us to reach for parent.parent.open directly
+        # A more elegant solution would implement a "datasource" interface on cacheable transformation blocks.
+        cached = self.store.rehash(path, self.parent.datasource.open(path, os.O_RDONLY))
+        if cached is not None:
+            return cached
+
+        return self.store.update(path, Passthrough.open(self, path, os.O_RDONLY))
